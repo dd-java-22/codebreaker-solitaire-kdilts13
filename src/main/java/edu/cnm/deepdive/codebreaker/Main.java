cb810409091c57dc4ff97e599075358f82a8ee3d
Kevin Dilts <kdilts@cnm.edu>
2026-02-10 08:28:34 -0700

    forgot to commit Main.java

--- src/main/java/edu/cnm/deepdive/codebreaker/Main.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/Main.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.codebreaker;
 
 import edu.cnm.deepdive.codebreaker.model.Game;
+import edu.cnm.deepdive.codebreaker.model.Guess;
 import edu.cnm.deepdive.codebreaker.service.CodebreakerService;
 import java.util.concurrent.CompletableFuture;
 
@@ -12,16 +13,35 @@ public class Main {
         .length(2);
 
     CodebreakerService service = CodebreakerService.getInstance();
-    CompletableFuture<Game> future = service.startGame(game);
+    CompletableFuture<Game> createGameFuture = service.startGame(game);
 
-    future
+    createGameFuture
         .thenAccept((startedGame) -> {
           System.out.println("Created a game: " + startedGame);
+          String gameId = startedGame.getId();
+
+          CompletableFuture<Game> getGameFuture = service.getGame(gameId);
+
+          getGameFuture
+            .thenAccept(fetchedGame -> {
+              System.out.println("fetched game: " + fetchedGame);
+
+              CompletableFuture<Guess> submitGuessFuture = service.submitGuess(gameId, new Guess().text("AB"));
+
+              submitGuessFuture
+                .thenAccept(submittedGuess -> {
+                  System.out.println("submitGuess: " + submittedGuess);
+                })
+                .exceptionally(Main::printTrace);
+            })
+            .exceptionally(Main::printTrace);
         })
-        .exceptionally((throwable) -> {
-          throwable.printStackTrace();
-          return null;
-        });
+        .exceptionally(Main::printTrace);
     System.out.println("game start requested");
+  }
+
+  private static Void printTrace(Throwable throwable) {
+    throwable.printStackTrace();
+    return null;
   }
 }
