f729a5ef77baecbaf68bbeb23e29800942343dea
Kevin Dilts <kdilts@cnm.edu>
2026-02-10 15:07:06 -0700

    starting to build game loop

--- src/main/java/edu/cnm/deepdive/codebreaker/Main.java
+++ src/main/java/edu/cnm/deepdive/codebreaker/Main.java
@@ -1,18 +1,53 @@
 package edu.cnm.deepdive.codebreaker;
 
+import edu.cnm.deepdive.codebreaker.model.Game;
 import edu.cnm.deepdive.codebreaker.viewmodel.GameViewModel;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
 
-  static void main() {
+  private final BlockingQueue<Game> updateQueue = new LinkedBlockingQueue<>();
+  private boolean solved;
+
+  void main() {
     GameViewModel viewModel = GameViewModel.getInstance();
 
-    viewModel.registerGameObserver(System.out::println);
+    viewModel.registerGameObserver((updateQueue::add));
+
     viewModel.registerGuessObserver(System.out::println);
     viewModel.registerErrorObserver(System.out::println);
 
     viewModel.startGame("ABCDEF", 2);
 
-    System.out.println("game start requested");
+    while (!solved) {
+      try {
+        Game game = updateQueue.take();
+
+        solved = Boolean.TRUE.equals(game.getSolved());
+
+        System.out.println("-------------------------------");
+        System.out.println("Guesses:");
+        //noinspection DataFlowIssue
+        game.getGuesses().forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Game:");
+        System.out.println(game);
+        System.out.println();
+        System.out.println("-------------------------------");
+
+        if (solved) {
+          System.out.println("You solved it!");
+        } else {
+          // TODO: 2/10/2026 get next guess from user
+          // TODO: 2/10/2026 submit new guess
+        }
+
+      } catch (InterruptedException e) {
+        //noinspection CallToPrintStackTrace
+        e.printStackTrace();
+      }
+    }
   }
 }
