55282e232d1f6a0d5ac5e78e8b97fb533ee917f8
Kevin Dilts <kdilts@cnm.edu>
2026-02-11 14:33:35 -0700

    Hello world in JavaFX

---
+++ src/main/java/edu/cnm/deepdive/codebreaker/controller/HelloController.java
@@ -0,0 +1,16 @@
+package edu.cnm.deepdive.codebreaker.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+public class HelloController {
+
+  @FXML
+  private Text greeting;
+
+  @FXML
+  private void initialize() {
+    greeting.setText("Hello, brave new world!");
+  }
+
+}
