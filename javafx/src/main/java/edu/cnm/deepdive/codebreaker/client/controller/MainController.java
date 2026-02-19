eb926dcd8ca03730afd2b1a11b0e8c589026d6c4
Kevin Dilts <kdilts@cnm.edu>
2026-02-19 16:38:26 -0700

    finished moving codePoint-to-name and -class lookups to CodePointInfo

--- javafx/src/main/java/edu/cnm/deepdive/codebreaker/client/controller/MainController.java
+++ javafx/src/main/java/edu/cnm/deepdive/codebreaker/client/controller/MainController.java
@@ -18,17 +18,11 @@ package edu.cnm.deepdive.codebreaker.client.controller;
 import edu.cnm.deepdive.codebreaker.api.model.Game;
 import edu.cnm.deepdive.codebreaker.api.model.Guess;
 import edu.cnm.deepdive.codebreaker.client.adapter.GuessAdapter;
+import edu.cnm.deepdive.codebreaker.client.util.CodePointInfo;
 import edu.cnm.deepdive.codebreaker.client.viewmodel.GameViewModel;
 import java.io.IOException;
 import java.net.URL;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.ResourceBundle;
-import java.util.function.Predicate;
-import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -38,14 +32,11 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Labeled;
 import javafx.scene.control.ListView;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.control.Toggle;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.Tooltip;
 import javafx.scene.layout.TilePane;
-import javafx.scene.text.Text;
-import javafx.scene.text.TextFlow;
 
 /**
  * Handles UI interactions and updates for the Codebreaker game. This class is responsible for
@@ -56,13 +47,10 @@ import javafx.scene.text.TextFlow;
 public class MainController {
 
   private static final String POOL_KEY = "pool";
-  private static final String POOL_NAMES_KEY = "pool_names";
-  private static final String POOL_CLASSES_KEY = "pool_classes";
   private static final String LENGTH_KEY = "length";
   private static final String GUESS_ITEM_LAYOUT_KEY = "guess_item_layout";
   private static final String PALETTE_ITEM_LAYOUT_KEY = "palette_item_layout";
   private static final char MNEMONIC_PREFIX = '_';
-  private static final Pattern PROPERTY_LIST_DELIMITER = Pattern.compile("\\s*,\\s*");
 
   @FXML
   private ResourceBundle resources;
@@ -79,8 +67,6 @@ public class MainController {
   private Game game;
   private String pool;
   private int length;
-  private Map<Integer, String> codePointNames;
-  private Map<Integer, String> codePointClasses;
   private URL paletteItemUrl;
   private URL guessItemUrl;
   private ToggleGroup group;
@@ -120,22 +106,6 @@ public class MainController {
   private void loadGameProperties() {
     pool = resources.getString(POOL_KEY);
     length = Integer.parseInt(resources.getString(LENGTH_KEY));
-    List<Integer> poolCodePoints = pool
-        .codePoints()
-        .boxed()
-        .toList();
-    List<String> poolNames = splitToList(resources.getString(POOL_NAMES_KEY));
-    List<String> poolClasses = splitToList(resources.getString(POOL_CLASSES_KEY));
-    codePointNames = new LinkedHashMap<>();
-    codePointClasses = new LinkedHashMap<>();
-    Iterator<Integer> codePointIter = poolCodePoints.iterator();
-    Iterator<String> nameIter = poolNames.iterator();
-    Iterator<String> classIter = poolClasses.iterator();
-    while (codePointIter.hasNext() && nameIter.hasNext() && classIter.hasNext()) {
-      Integer codePoint = codePointIter.next();
-      codePointNames.put(codePoint, nameIter.next());
-      codePointClasses.put(codePoint, classIter.next());
-    }
     paletteItemUrl = getClass().getResource(resources.getString(PALETTE_ITEM_LAYOUT_KEY));
     guessItemUrl = getClass().getResource(resources.getString(GUESS_ITEM_LAYOUT_KEY));
   }
@@ -150,15 +120,8 @@ public class MainController {
     viewModel.startGame(pool, length);
   }
 
-  private List<String> splitToList(String joined) {
-    return PROPERTY_LIST_DELIMITER
-        .splitAsStream(joined)
-        .filter(Predicate.not(String::isEmpty))
-        .toList();
-  }
-
   private void handleGame(Game game) {
-    // TODO: Add logic to handle null gamee reference (e.g., after deleting current game).
+    // TODO: Add logic to handle null game reference (e.g., after deleting current game).
     this.game = game;
 
     updateGuessHistory();
@@ -178,10 +141,9 @@ public class MainController {
   private void buildPalette() {
     ObservableList<Node> children = guessPalette.getChildren();
     children.clear();
-    codePointClasses
-        .entrySet()
-        .stream()
-        .map(this::buildPaletteItem)
+    pool
+        .codePoints()
+        .mapToObj(this::buildPaletteItem)
         .forEach(children::add);
 //    guessPalette.setMaxWidth(Region.USE_PREF_SIZE);
   }
@@ -199,16 +161,16 @@ public class MainController {
     send.setDisable(sendDisabled);
   }
 
-  private Labeled buildPaletteItem(Entry<Integer, String> entry) {
+  private Labeled buildPaletteItem(int codePoint) {
     try {
-      Integer key = entry.getKey();
-      String name = codePointNames.get(key);
+      CodePointInfo info = CodePointInfo.getInstance();
+      String name = info.getName(codePoint);
       Labeled node = new FXMLLoader(paletteItemUrl, resources).load();
       node.addEventHandler(ActionEvent.ACTION, this::handlePaletteItemSelection);
       node.setTooltip(new Tooltip(name));
       node.setText(buildSingleCharacterMnemonicLabel(name));
-      node.setUserData(key);
-      node.getStyleClass().add(entry.getValue());
+      node.setUserData(codePoint);
+      node.getStyleClass().add(info.getStyleClass(codePoint));
       return node;
     } catch (IOException e) {
       throw new RuntimeException(e);
@@ -252,7 +214,7 @@ public class MainController {
     button.setUserData(codePoint);
     ObservableList<String> styleClasses = button.getStyleClass();
     styleClasses.subList(1, styleClasses.size()).clear();
-    styleClasses.add(codePointClasses.get(codePoint));
+    styleClasses.add(CodePointInfo.getInstance().getStyleClass(codePoint));
     ObservableList<Toggle> toggles = group.getToggles();
     int position = toggles.indexOf(button);
     if (position < toggles.size() - 1) {
@@ -276,7 +238,7 @@ public class MainController {
   private ToggleButton buildGuessItem(int[] lastGuess, int i) {
     try {
       ToggleButton button = new FXMLLoader(guessItemUrl, resources).load();
-      String styleClass = codePointClasses.get(lastGuess[i]);
+      String styleClass = CodePointInfo.getInstance().getStyleClass(lastGuess[i]);
       if (styleClass != null) {
         button.getStyleClass().add(styleClass);
         button.setUserData(lastGuess[i]);
