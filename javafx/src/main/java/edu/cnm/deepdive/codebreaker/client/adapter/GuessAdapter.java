23317260dfee717e9132359fe23712f927520089
Kevin Dilts <kdilts@cnm.edu>
2026-02-19 17:00:30 -0700

    implemented GuessAdapter method to add guess items to history display - no styles

--- javafx/src/main/java/edu/cnm/deepdive/codebreaker/client/adapter/GuessAdapter.java
+++ javafx/src/main/java/edu/cnm/deepdive/codebreaker/client/adapter/GuessAdapter.java
@@ -1,14 +1,19 @@
 package edu.cnm.deepdive.codebreaker.client.adapter;
 
 import edu.cnm.deepdive.codebreaker.api.model.Guess;
+import edu.cnm.deepdive.codebreaker.client.util.CodePointInfo;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.util.Callback;
@@ -25,7 +30,8 @@ public class GuessAdapter implements Callback<ListView<Guess>, ListCell<Guess>> 
   public GuessAdapter(ResourceBundle resources) {
     this.resources = resources;
     item_layout_location = getClass().getResource(resources.getString(GUESS_HISTORY_LAYOUT_KEY));
-    character_layout_location = getClass().getResource(resources.getString(GUESS_CHARACTER_LAYOUT_KEY));
+    character_layout_location = getClass().getResource(
+        resources.getString(GUESS_CHARACTER_LAYOUT_KEY));
   }
 
   @Override
@@ -74,11 +80,34 @@ public class GuessAdapter implements Callback<ListView<Guess>, ListCell<Guess>> 
         setGraphic(null);
       } else {
         // TODO: 2/19/2026 manipulate nodes referenced by @FXML annotated fields to present model state
+        List<Node> children = guessContent.getChildren();
+        children.clear();
+
+        CodePointInfo info = CodePointInfo.getInstance();
+
+        guess.getText().codePoints()
+            .mapToObj((codePoint) -> {
+              return buildGuessCharacterItem(codePoint, info);
+            })
+            .forEach(children::add);
 
         exactCount.setText(String.valueOf(guess.getExactMatches()));
         nearCount.setText(String.valueOf(guess.getNearMatches()));
 
         setGraphic(root);
+      }
+    }
+
+    private Label buildGuessCharacterItem(int codePoint, CodePointInfo info) {
+      try {
+        FXMLLoader loader = new FXMLLoader(character_layout_location, resources);
+        Label label = loader.load();
+        label.setUserData(codePoint);
+        label.setTooltip(new Tooltip(info.getName(codePoint)));
+        label.getStyleClass().add(info.getStyleClass(codePoint));
+        return label;
+      } catch (IOException e) {
+        throw new RuntimeException(e);
       }
     }
   }
