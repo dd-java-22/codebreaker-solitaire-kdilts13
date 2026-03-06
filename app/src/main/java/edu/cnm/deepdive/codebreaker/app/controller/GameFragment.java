09fe26e4962de351eaaaffb9695c01ecab30ef8f
Kevin Dilts <kdilts@cnm.edu>
2026-03-06 09:23:25 -0700

    Enhance GameFragment: Add ViewModel observers and update layout styles

--- app/src/main/java/edu/cnm/deepdive/codebreaker/app/controller/GameFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/app/controller/GameFragment.java
@@ -1,25 +1,20 @@
 package edu.cnm.deepdive.codebreaker.app.controller;
 
-import android.content.Context;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
 import dagger.hilt.android.AndroidEntryPoint;
-import edu.cnm.deepdive.codebreaker.api.model.Game;
-import edu.cnm.deepdive.codebreaker.app.adapter.GuessAdapter;
 import edu.cnm.deepdive.codebreaker.app.databinding.FragmentGameBinding;
 import edu.cnm.deepdive.codebreaker.app.util.SymbolMap;
 import edu.cnm.deepdive.codebreaker.app.viewmodel.GameViewModel;
 import jakarta.inject.Inject;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 @AndroidEntryPoint
 public class GameFragment extends Fragment {
@@ -33,9 +28,58 @@ public class GameFragment extends Fragment {
   @Nullable
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-      @Nullable Bundle savedInstanceState) {
+    @Nullable Bundle savedInstanceState) {
     binding = FragmentGameBinding.inflate(inflater, container, false);
     return binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    gameViewModel = new ViewModelProvider(requireActivity()).get(GameViewModel.class);
+
+    LifecycleOwner lifecycleOwner = getViewLifecycleOwner();
+
+    gameViewModel.getGame()
+      .observe(lifecycleOwner, game -> {
+        // TODO: 3/6/2026 handle updates to game
+
+        binding.palette.removeAllViews();
+
+        game.getPool().codePoints()
+          .mapToObj(codePoint -> {
+            // TODO inflate a layout and return inflated widget
+            return (ImageButton) null;
+          })
+          .map(symbolWidget -> {
+            // TODO set the symbol text (contentDescription and tooltip)
+            return symbolWidget;
+          })
+          .map(symbolWidget -> {
+            // TODO set the symbol drawable
+            return symbolWidget;
+          })
+          .map(symbolWidget -> {
+            // TODO set the symbol drawable's tint
+            return symbolWidget;
+          })
+          .forEach(binding.palette::addView);
+      });
+
+    gameViewModel.getSolved()
+      .observe(lifecycleOwner, solved -> {
+        // TODO: 3/6/2026 handle changes to solved state of game
+      });
+
+    gameViewModel.getGuess()
+      .observe(lifecycleOwner, guess -> {
+        // TODO: 3/6/2026 handle updates to most recent guess
+      });
+
+    gameViewModel.getError()
+      .observe(lifecycleOwner, error -> {
+        // TODO: 3/6/2026 handle error
+      });
   }
 
   @Override
